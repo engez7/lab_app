83d3f3b232ea4e1b0122ef16d81b33fa871e4521
ezio recaldini <ezio.recaldini@gmail.com>
2024-01-26 14:01:19 +0100

    time.sleep(60) after MQTT message reception Now env_log.py can run in background called only at boot time (not cron every xx minutes)

--- env_log.py
+++ env_log.py
@@ -1,51 +1,7 @@
-'''
-#OLD-RIMUOVERE SE OK
-#Questo programma viene lanciato ogni 10 minuti tramite cron (sudo)
-
 import paho.mqtt.client as mqtt
 import sqlite3
-import sys #timestamp, NOTE:UTC on raspez server (default)
 import time
-
-MQTT_SERVER = "192.168.1.254" # localhost is MQTT server/broker
-MQTT_TOPIC_TEMPHUM = b"esp/dht/TempHum_z01" #temperature and humidity zone 01
-
-def log_values(sensor_id, temp, hum):
-    conn=sqlite3.connect('/var/www/lab_app/lab_app.db')
-    curs=conn.cursor()
-    curs.execute("""INSERT INTO temphum values(datetime(CURRENT_TIMESTAMP, 'localtime'), (?), (?), (?))""", (sensor_id,temp,hum))
-    conn.commit()
-    conn.close()
-
-def on_connect(client, userdata, flags, rc):
-    print("connected OK")
-    client.subscribe(MQTT_TOPIC_TEMPHUM, qos =1)
-
-def on_message(client, userdata, msg):
-    if msg.topic == MQTT_TOPIC_TEMPHUM:
-        print(msg.topic+" "+str(msg.payload))
-
-        values = msg.payload.split(",") # message is a string like: 18.4,60.2
-        temperature = float(values[0])
-        humidity = float(values[1])
-        print("{:.2f}".format(temperature) + "," + "{:.2f}".format(humidity))
-        log_values("DHT_01", temperature, humidity)
-
-client = mqtt.Client("python_pahoMQTT_lab_app") #create new instance
-
-client.on_connect = on_connect  #bind call back functions
-client.on_message = on_message
-
-print("Connecting to broker ",MQTT_SERVER)
-client.connect(MQTT_SERVER)      #connect to broker
-
-client.loop_forever()
-
-#MOD1 to test VSCode --> GitHUb
-'''
-import paho.mqtt.client as mqtt
-import sqlite3
-import sys
+#import sys
 
 MQTT_SERVER = "192.168.1.254"
 MQTT_TOPIC_TEMPHUM = b"esp/dht/TempHum_z01"
@@ -75,9 +31,12 @@ def on_message(client, userdata, msg):
         print("{:.2f}".format(temperature) + "," + "{:.2f}".format(humidity))
         log_values("DHT_01", temperature, humidity)
 
+        # Wait 10 minutes before return to listening status
+        time.sleep(60) #Testing with 1 min...
+        
         # Chiudi il programma dopo aver gestito il messaggio
-        client.disconnect()
-        sys.exit()
+        #client.disconnect()
+        #sys.exit()
 
 client = mqtt.Client("python_pahoMQTT_lab_app")
 client.on_connect = on_connect
